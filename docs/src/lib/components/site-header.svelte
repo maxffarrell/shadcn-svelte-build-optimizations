225d2b99088c53054475d1feb20a276b97c588f0
Max Farrell <maxffarrell@gmail.com>
2026-09-12 15:37:24 -0500

    fix: remove obsolete Typeset header customizer

--- docs/src/lib/components/site-header.svelte
+++ docs/src/lib/components/site-header.svelte
@@ -60,8 +60,6 @@
 							Initialize Project
 						</Button>
 					{/if}
-				{:else if page.url.pathname === "/typeset"}
-					<Customizer />
 				{:else}
 					<ModeSwitcher />
 					<Button href="/create" variant="default" size="sm" class="h-[31px] rounded-lg">
